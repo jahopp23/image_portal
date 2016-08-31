4eab45c8e72df9ee5853d480c00ffe0f1d358fc3
jahopp23 <jahopp@gmail.com>
2016-08-31 15:30:19 -0500

    init

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,21 @@
+class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+  def create 
+    @image = Image.new(image_params)
+    if @image.save 
+      redirect_to action: 'index'
+    else
+      redirect_to action: 'new'
+    end
+  end
+private
+  def image_params
+   params.require(:image).permit( :title, :description, :avatar)
+  end
+end
